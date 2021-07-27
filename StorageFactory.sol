1111455a6893ac8b79ef6366f0d8a274e01fdd2d
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-07-27 01:27:06 -0400

    updated storage factory

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License_Identifier: MIT 
 
 pragma solidity ^0.6.0;
 
-import "./SimpleStorage.sol";
+import "./SimpleStorage.sol"; 
 
 contract StorageFactory is SimpleStorage {
     
@@ -13,11 +13,13 @@ contract StorageFactory is SimpleStorage {
         simpleStorageArray.push(simpleStorage);
     }
     
-    function ssStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
         SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
     }
     
-    function ssGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-    } 
+    }
 }
