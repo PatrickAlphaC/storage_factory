0e102738c30241e3052a32ab430e3f85fa8655bf
jdecrypted <92306421+jdecrypted@users.noreply.github.com>
2021-10-14 16:25:01 +0530

    Update StorageFactory.sol
    SimpleStorage objects can be directly accessed from given array, No need to explicitly cast in another object.

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -16,10 +16,10 @@ contract StorageFactory is SimpleStorage {
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         // Address 
         // ABI 
-        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
     
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
